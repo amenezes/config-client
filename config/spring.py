82efa0726aeb43fcd9adea7d9f2e8841424fd170
amenezes <alexandre.fmenezes@gmail.com>
2019-03-22 09:59:48 -0300

    code refactoring.

--- config/spring.py
+++ config/spring.py
@@ -21,36 +21,40 @@ class ConfigServer:
     def __load_config_from_configserver(self):
         """Retrieve configuration information."""
         try:
-            logging.info('Retrieving config server configuration...')
-
             request_url = self.__format_configserver_url()
             response = requests.get(request_url)
 
-            logging.debug(request_url)
             logging.debug(f"Response status code: {response.status_code}")
 
             if response.status_code == 200:
                 self._config = response.json()
 
         except requests.exceptions.ConnectionError:
-            logging.error('Failed to establish connection with configserver.')
+            logging.error(
+                'Failed to establish connection with configserver.')
             sys.exit(1)
 
     def __format_configserver_url(self):
         """Format ConfigServer URL."""
-        logging.debug('Requesting configuration file')
+        logging.debug('Building request URL')
 
         address = os.getenv('CONFIGSERVER_ADDRESS') or 'http://localhost:8888/configuration'
         branch = os.getenv('BRANCH') or 'master'
         profile = os.getenv('PROFILE') or 'development'
         context_name = os.getenv('APP_NAME')
 
+        logging.debug(f"ConfigServer address: {address}")
+        logging.debug(f"Branch configuration: {branch}")
+        logging.debug(f"Profile set: {profile}")
+        logging.debug(f"Configuration file base name: {context_name}")
+        logging.debug(
+            f"URL formatted: {address}/{branch}/{context_name}-{profile}.json")
+
         return f"{address}/{branch}/{context_name}-{profile}.json"
 
     @property
     def config(self):
         """Getter from configurations retrieved from configserver."""
-        logging.debug('Getting atribute from content')
         return self._config
 
     def get_attribute(self, attr):
@@ -59,17 +63,18 @@ class ConfigServer:
         Use <dot> to define a path on a key tree.
         """
         key_list = attr.split('.')
-        atribute_content = self._config.get(key_list[0])
+        logging.debug(f'Key attribute: {key_list}')
+
+        attribute_content = self.config.get(key_list[0])
+        logging.debug(f'Attribute content: {attribute_content}')
 
         for key in key_list[1:]:
-            atribute_content = atribute_content.get(key)
+            attribute_content = attribute_content.get(key)
 
-        logging.debug('Getting atribute from content')
+        logging.debug(f"Configuration getted: {attribute_content}")
 
-        return atribute_content
+        return attribute_content
 
     def get_keys(self):
         """List all keys from configuration retrieved."""
-        logging.debug('Gettings keys from config file')
-
-        return self.config().keys()
+        return self.config.keys()
