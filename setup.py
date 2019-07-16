380b730d05d76e033596f4e74983aafa7b506b95
alexandre menezes <alexandre.fmenezes@stf.jus.br>
2019-07-16 15:20:02 -0300

    adjusments for release 0.3.x

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-import setuptools
-
 from collections import OrderedDict
+
+import setuptools
 
 
 with open("README.md", "r") as fh:
@@ -8,24 +8,26 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="config-client",
-    version="0.2.0",
+    version="0.3.0",
     author="alexandre menezes",
     author_email="alexandre.fmenezes@gmail.com",
     description="config service client",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license='Apache-2.0',
     url="https://github.com/amenezes/config-client",
     packages=setuptools.find_packages(),
+    python_requires='>=3.6.0',
     project_urls=OrderedDict((
         ('Documentation', 'https://github.com/amenezes/config-client'),
         ('Code', 'https://github.com/amenezes/config-client'),
         ('Issue tracker', 'https://github.com/amenezes/config-client/issues')
     )),
     install_requires=[
-        'requests>=2.22.0'
+        'requests>=2.22.0',
+        'attrs>=19.1.0'
     ],
     classifiers=[
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Framework :: AsyncIO",
