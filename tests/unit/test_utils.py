2ec26545a0cf6930f944270f4b04e79d38b83ebc
Luis Fernando Gomes <dev@luiscoms.com.br>
2020-04-20 10:07:47 -0300

    Reformatting

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -4,14 +4,13 @@ from config.utils import merge_dicts
 
 
 class TestMerge(TestCase):
-
     def test_should_merge_deep_dicts(self):
         # given
-        a_dict = {'first': {'all_rows': {'pass': 'dog', 'number': '1'}}}
-        b_dict = {'first': {'all_rows': {'fail': 'cat', 'number': '5'}}}
+        a_dict = {"first": {"all_rows": {"pass": "dog", "number": "1"}}}
+        b_dict = {"first": {"all_rows": {"fail": "cat", "number": "5"}}}
         # when
         merged = dict(merge_dicts(a_dict, b_dict))
         # then
-        expected = {'first': {'all_rows': {'pass': 'dog', 'fail': 'cat', 'number': '5'}}}
+        expected = {"first": {"all_rows": {"pass": "dog", "fail": "cat", "number": "5"}}}
 
         self.assertEqual(expected, merged)
