cee23ebb3d8ce0849ee4f9318a8c94f88d6c490b
alexandre menezes <alexandre.fmenezes@gmail.com>
2022-10-26 11:25:57 -0300

    added support to python 3.11

--- config/__init__.py
+++ config/__init__.py
@@ -3,7 +3,7 @@ from .cf import CF
 from .cfenv import CFenv
 from .spring import ConfigClient, config_client, create_config_client
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __all__ = [
     "__version__",
     "ConfigClient",
