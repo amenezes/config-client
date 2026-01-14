98c847b886a5551f1f0848eb1d552bc05d1238e3
Alexandre Menezes <alexandre.fmenezes@stf.jus.br>
2026-01-14 00:10:36 -0300

    fix: using async function with FastAPI integration

--- config/ext/fastapi.py
+++ config/ext/fastapi.py
@@ -28,7 +28,7 @@ async def fastapi_config_client(request: Request):
     except AttributeError:
         logger.debug("Initializing ConfigClient")
         cc = ConfigClient()
-        cc.get_config()
+        await cc.get_config_async()
         request.app.config_client = cc
         logger.debug("ConfigClient successfully initialized")
 
@@ -56,6 +56,6 @@ async def fastapi_cloud_foundry(request: Request):
     except AttributeError:
         logger.debug("Initializing ConfigClient")
         cc = CF()
-        cc.get_config()
+        await cc.get_config_async()
         request.app.config_client = cc
         logger.debug("ConfigClient successfully initialized")
